20fe79b2fa3b5f99f247864ed2d78c05a7632470
JosueDev22 <businessjop22@gmail.com>
2024-06-28 11:23:39 -0300

    Aula 28.06.24

---
+++ Criando um Formulário Django 2/apps/repouso/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path("", VerPacientes, name="pagina_inicial"),
+    path("editar-paciente/<int:id_paciente>/", EditarPaciente, name='pg_editar_paciente')
+]
